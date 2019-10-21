77856ad7fefba3328b41100a788ddc286984bfad
Pavel-Liteiniy <paulsev96@gmail.com>
2019-10-21 22:10:27 +0300

    Исправление недочетов после первой проверки проекта
    Б4. Грубые ошибки в разметке.
    Д2. Не использовано минимально возможное количество HTML-элементов (нет лишних элементов) - лишняя обертка
    Б8. В CSS есть !important.
    Б9. Подключены неправильные цвета color - цвет текста не соответствует макету
    Д7. Есть вложенность селекторов больше двух.
    Д8. При использовании блочно-строчных элементов не указывал вертикальное выравнивание
    Д11. внешний вид при клике не соответствует макету
    Д25. Есть глобальные стили тегов.
    Д19. Вёрстка не проходит тест на переполнение контентом.
    Д17. нет анимации при ошибке
    Д26. нет активного состояния при фокусе с клавиатуры на форме внутренней страницы

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 var search_form = document.querySelector(".search-form");
 var access_button = document.querySelector(".access-button");
+var form = search_form.querySelector("form");
+var arrival_date = form.querySelector("[id=arrival-date]");
+var departure_date = form.querySelector("[id=date-of-departure]");
+var adult_guest = form.querySelector("[id=adult-guest]");
+
 
 access_button.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	search_form.classList.toggle("hide-search-form");
 	search_form.classList.toggle("show-search-form");
+	search_form.classList.remove("error-search-form");
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!arrival_date.value || !departure_date.value || !adult_guest.value) {
+      evt.preventDefault();
+      search_form.classList.remove("error-search-form");
+      search_form.offsetWidth = search_form.offsetWidth;
+      search_form.classList.add("error-search-form");
+    }
 });
